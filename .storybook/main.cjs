b65d5d38afe6b2fb94374b8fae3a1913b48799cb
Margarida Silva <ana.silva@replai.io>
2022-12-29 12:21:05 +0000

    feat: bump dependencies to storybook 7.0 beta

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,23 +1,28 @@
 const preprocess = require('svelte-preprocess');
 
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-svelte-csf"
   ],
-  "framework": "@storybook/svelte",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/svelte",
+  core: {
+    builder: "@storybook/builder-vite"
   },
-  "svelteOptions": {
-    "preprocess": preprocess()
+  svelteOptions: {
+    preprocess: preprocess({
+      typescript: true,
+      postcss: true,
+      sourceMap: true,
+    }),
   },
-  "features": {
-    "storyStoreV7": false
+  features: {
+    storyStoreV7: false
   }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import Decorator from './Decorator.svelte';
 
 export const decorators = [() => ({ Component: Decorator })];
+// export const decorators = [() => Decorator];
 
 export const parameters = {}
